143d07ad79ac357580991b7adea2d4262b505b32
Dr. Bleep <drbleep@bleeplabs.com>
2022-02-07 19:25:29 -0600

    4

--- Class_4/Class_4-etch_a_sketch_4_random_line/Class_4-etch_a_sketch_4_random_line.ino
+++ Class_4/Class_4-etch_a_sketch_4_random_line/Class_4-etch_a_sketch_4_random_line.ino
@@ -66,9 +66,26 @@ void setup() {
   pinMode(button3_pin, INPUT_PULLUP);
   debouncer3.attach(button3_pin);
   debouncer3.interval(10); // interval in ms
-
+  randomSeed(analogRead(A9));
   analogReadAveraging(64);  //smooth the readings so they jump around less
-}
+
+  int r1 = random(0, 8);
+  int r2 = random(0, 8);
+  int r3 = random(0, 8);
+  int r4 = random(0, 8);
+  float hue1 = random(100) / 100.0;
+  for ( int x_count = 0; x_count < 8; x_count++) {
+    for ( int y_count = 0; y_count < 8; y_count++) {
+      xy_count = x_count + (y_count * 8); //goes from 0-63
+
+      if (maskLine(xy_count, r1, r2, r3, r4)) {
+        bank1[y_count][x_count] = 1.0 + hue1;
+      }
+
+    }
+  }
+
+}//end of setup
 
 
 void loop() {
@@ -124,7 +141,7 @@ void loop() {
       shake = 1;
     }
   }
-  
+
   if (save == 1) {
     bank1[y_pot][x_pot] = save_hue + 1;
     save = 0;
@@ -178,6 +195,8 @@ void loop() {
   }
 
 }// loop is over
+
+
 
 
 
@@ -247,4 +266,163 @@ void set_pixel(int pixel, float fh, float fs, float fv) {
       break;
   }
   leds.setPixelColor(pixel, RedLight, GreenLight, BlueLight);
+}
+
+#ifndef _swap_int16_t
+#define _swap_int16_t(a, b)                                                    \
+  {                                                                            \
+    int16_t t = a;                                                             \
+    a = b;                                                                     \
+    b = t;                                                                     \
+  }
+#endif
+
+// Will return a 1 if there is a line at that point devined by the parameters
+// maskLine(pixel selected, First x, first y, destination x, destinationy)
+// based on https://github.com/adafruit/Adafruit-GFX-Library/blob/0b6068e833b050fa83ed90558c54f3389d26bc5b/Adafruit_GFX.cpp#L132
+
+byte maskLine(int16_t sel, int16_t x0, int16_t y0, int16_t x1, int16_t y1) {
+  byte pixels[64];
+  x0 = constrain(x0, 0, 7);
+  x1 = constrain(x1, 0, 7);
+  y0 = constrain(y0, 0, 7);
+  y1 = constrain(y1, 0, 7);
+  pixels[sel] = 0;
+
+  int16_t steep = abs(y1 - y0) > abs(x1 - x0);
+  if (steep) {
+    _swap_int16_t(x0, y0);
+    _swap_int16_t(x1, y1);
+  }
+
+  if (x0 > x1) {
+    _swap_int16_t(x0, x1);
+    _swap_int16_t(y0, y1);
+  }
+
+  int16_t dx, dy;
+  dx = x1 - x0;
+  dy = abs(y1 - y0);
+
+  int16_t err = dx >> 1;
+  int16_t ystep;
+
+  if (y0 < y1) {
+    ystep = 1;
+  } else {
+    ystep = -1;
+  }
+
+  for (; x0 <= x1; x0++) {
+    if (steep) {
+      int xyc = y0 + (x0 << 3);
+      //writePixel(y0, x0, color);
+      pixels[xyc] = 1;
+      //return xyc;
+      //set_pixel(xyc, 0, 0, 1);
+    } else {
+      int xyc = x0 + (y0 << 3);
+      //return xyc;
+      pixels[xyc] = 1;
+      //set_pixel(xyc, 0, 0, 1);
+      //writePixel(x0, y0, color);
+    }
+    err -= dy;
+    if (err < 0) {
+      y0 += ystep;
+      err += dx;
+    }
+  }
+
+  if (pixels[sel] == 1) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+
+}
+//three lines using three pairs of points. returns a 1 if the pixel select is on the triangles lines
+//  maskTriangle(pixel selected, x0, y0, x1,y1,x2,y2) {
+//  You can enable fill but right now it takes too long to up;date fater than 10fps
+byte maskTriangle(int sel, int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2) {
+  byte p[64];
+  byte a = maskLine(sel, x0, y0, x1,  y1);
+  byte b = maskLine(sel, x1, y1, x2,  y2);
+  byte c = maskLine(sel, x2, y2, x0,  y0);
+  byte f = a + b + c;
+  p[sel] = 0;
+  if (f > 0) {
+    p[sel] = 1;
+  }
+
+  byte pr, r;
+  int open1 = 0;
+  int close1 = 0;
+
+  if (0) { //enable for fill.  this takes like 70ms to do so can't update at 30 fps
+    for (byte xx = 0; xx < 8; xx++) {
+      for (byte j = 0; j < 8; j++) {
+        byte cs = xx + (j << 3);
+        byte a = maskLine(cs, x0, y0, x1,  y1);
+        byte b = maskLine(cs, x1, y1, x2,  y2);
+        byte c = maskLine(cs, x2, y2, x0,  y0);
+        byte f = a + b + c;
+        p[cs] = 0;
+        if (f > 0) {
+          p[cs] = 1;
+        }
+      }
+      for (byte yy = 1; yy < 8; yy++) {
+
+        int cxy = xx + (yy * 8);
+        int pxy = (xx) + ((yy - 1) * 8);
+        if (p[pxy] > 0 && p[cxy] == 0) {
+          open1 = yy;
+          //Serial.print(" !");          Serial.println(open1);
+        }
+        if (open1 > 0) {
+          if (p[pxy] == 0 && p[cxy] > 0) {
+            close1 = yy;
+          }
+
+        }
+        if (close1 > 0 && open1 < close1) {
+          for (int yf = open1; yf < close1; yf++) {
+            p[xx + (yf * 8)] = 1;
+          }
+        }
+      }
+    }
+  }
+
+  if (p[sel] > 0) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+//four lines using four pairs of points. returns a 1 if the pixel select is on the triangles lines
+//  maskRectangle(pixel selected, x0, y0, x1,y1,x2,y2,x3,y3) {
+
+byte maskPolygon(int sel, int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, int16_t x3, int16_t y3) {
+  byte p[64];
+  byte a = maskLine(sel, x0, y0, x1,  y1);
+  byte b = maskLine(sel, x1, y1, x2,  y2);
+  byte c = maskLine(sel, x2, y2, x3,  y3);
+  byte d = maskLine(sel, x3, y3, x0,  y0);
+  byte f = a + b + c + d;
+  p[sel] = 0;
+  if (f > 0) {
+    p[sel] = 1;
+  }
+
+  if (p[sel] > 0) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
 }
